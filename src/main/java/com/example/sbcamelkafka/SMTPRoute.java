52bde34fc8b3ac6a1ed2ad14e8703685b1b78b09
Abhishek Vishnoi <abvishno@redhat.com>
2023-08-22 14:45:49 +0530

    first commit

--- src/main/java/com/example/sbcamelkafka/SMTPRoute.java
+++ src/main/java/com/example/sbcamelkafka/SMTPRoute.java
@@ -1,7 +1,14 @@
 package com.example.sbcamelkafka;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.attachment.AttachmentMessage;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
+
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+import java.io.File;
 
 @Component
 public class SMTPRoute extends RouteBuilder {
@@ -15,8 +22,22 @@ public class SMTPRoute extends RouteBuilder {
                 .setHeader("To").jsonpath("$.message.toEmail")
                 .setHeader("Subject").jsonpath("$.message.subject")
                 .setBody(jsonpath("$.message.body"))
+                .process(new Processor() {
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        AttachmentMessage attMsg = exchange.getIn(AttachmentMessage.class);
+
+                       // File file = new File("classpath:sample.ics");
+
+                        File file = new File("/Users/abvishno/workspaces/indigo/email-notification-service/src/main/resources/sample.ics");
+
+                        attMsg.addAttachment("message1.xml",
+                                new DataHandler(new FileDataSource(file)));
+
+                        exchange.getIn().getBody();
+                    }
+                })
                 .to("smtp://smtp.freesmtpservers.com:25");
-
-
+        
     }
 }
