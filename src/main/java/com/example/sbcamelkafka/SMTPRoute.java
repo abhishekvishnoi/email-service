e4089dbc9cf3c10d4de69b1c72ba88ae0acc5e68
Abhishek Vishnoi <abvishno@redhat.com>
2023-08-24 13:00:26 +0530

    first commit

--- src/main/java/com/example/sbcamelkafka/SMTPRoute.java
+++ src/main/java/com/example/sbcamelkafka/SMTPRoute.java
@@ -1,5 +1,6 @@
 package com.example.sbcamelkafka;
 
+import com.sun.istack.ByteArrayDataSource;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.attachment.AttachmentMessage;
@@ -23,22 +24,40 @@ public class SMTPRoute extends RouteBuilder {
         Processor processor = new Processor() {
             @Override
             public void process(Exchange exchange) throws Exception {
+
+              String content =  exchange.getIn().getHeader("ics" , String.class);
+
                 AttachmentMessage attMsg = exchange.getIn(AttachmentMessage.class);
+
+              //  String content = "This is the content of the string.";
+
+                // Convert the string to a byte array
+                byte[] byteArray = content.getBytes();
+
+                // Create a DataSource from the byte array
+                DataSource dataSource = new ByteArrayDataSource(byteArray, "text/plain");
+
+                // Create a DataHandler from the DataSource
+                DataHandler dataHandler = new DataHandler(dataSource);
+
+
+                //InputStream input = getClass().getResourceAsStream("/sample.ics");
                 Resource resource = new ClassPathResource("sample.ics");
-                InputStream input = resource.getInputStream();
                 File file = resource.getFile();
                 attMsg.addAttachment("meeting-invite",
-                        new DataHandler(new FileDataSource(file)));
+                        new DataHandler(dataSource));
+
 
 
              //   DataSource ds = new
-
               //  exchange.getIn().getBody();
             }
         };
 
         from("kafka:{{topic}}?brokers={{broker}}")
                 .log("Message received from Kafka : ${body} on the topic ${headers[kafka.TOPIC]}")
+               // .log("{{mail.ics}}")
+                .setHeader("ics").simple("{{mail.ics}}")
                 .setHeader("From").jsonpath("$.message.fromEmail")
                 .setHeader("To").jsonpath("$.message.toEmail")
                 .setHeader("Subject").jsonpath("$.message.subject")
